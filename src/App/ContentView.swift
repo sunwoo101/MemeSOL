6e6d19fe2d46a3ce0136afc36752b5faab47d940
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2026-04-24 13:51:46 +1000

    Initialise Project

---
+++ src/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment3
+//
+//  Created by Sun Woo Kim on 24/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/App/Main.swift
@@ -0,0 +1,17 @@
+//
+//  Main.swift
+//  Assignment3
+//
+//  Created by Sun Woo Kim on 24/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
