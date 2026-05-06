10199f727ca84ceac646dcb4377e0f2beccd98a1
gk <198441850+02preet@users.noreply.github.com>
2026-05-06 22:38:40 +1000

    feat: style sendview

--- apps/ios/src/App/SendView.swift
+++ apps/ios/src/App/SendView.swift
@@ -8,18 +8,57 @@
 import SwiftUI
 
 struct SendView: View {
-    @State var text = ""
+    @State var address = ""
+    @State var amount = ""
+    @State var selectedToken = ""
     
     
     var body: some View {
-        VStack {
-            TextField("Enter Recepient Address...", text: $text)
-                .padding()
-                .background(AppColors.charcoalColor)
-                .foregroundColor(.white)
+        ZStack {
+            AppColors.blackColor.ignoresSafeArea()
             
-            Text("Hello world!")
-
+            VStack (spacing: 20) {
+                Text("Send")
+                    .font(.title2.bold())
+                    .foregroundColor(AppColors.goldColor)
+                    .padding(.top, 10)
+                
+                VStack (alignment: .leading, spacing: 16) {
+                    Text("Recipient Address")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    HStack {
+                        TextField("Enter or scan wallet address", text: $address, axis: .vertical)
+                            .foregroundColor(.white)
+                            
+                        
+                        Spacer()
+                        
+                        Divider()
+                                .frame(height: 45)
+                                .background(Color.white.opacity(0.9))
+                        
+                        Button (action: {
+                            //do scan address
+                        }) {
+                            VStack(spacing: 5) {
+                                Image(systemName: "qrcode.viewfinder")
+                                Text("Scan")
+                                    .font(.system(size:12))
+                            }
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 5)
+                        }
+                    }
+                    .padding()
+                    .background(AppColors.charcoalColor)
+                    .cornerRadius(SharedLayout.cornerRadius)
+                    
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 10)
         }
     }
 }
