2b2913e1625153d6a8cfe41ef7409b5b01d2f9a6
gk <198441850+02preet@users.noreply.github.com>
2026-05-08 17:21:43 +1000

    feat: create createtokenview layout

--- apps/ios/src/App/CreateTokenView.swift
+++ apps/ios/src/App/CreateTokenView.swift
@@ -8,8 +8,106 @@
 import SwiftUI
 
 struct CreateTokenView: View {
+    @State private var name = ""
+    @State private var symbol = ""
+    @State private var supply = ""
+    @State private var image: Data?
+    
+    @StateObject private var viewModel = CreateTokenViewModel()
+    
+    //check there are values for name, symbol, image and supply
+    //if these conditions are met -> variable equals true
+    private var isFormValid: Bool {
+        !name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+        !symbol.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+        !supply.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+        image != nil
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AppColors.blackColor.ignoresSafeArea()
+            
+            VStack (spacing: 20) {
+                Text("Create Token")
+                    .font(.title2.bold())
+                    .foregroundColor(AppColors.goldColor)
+                    .padding(.top, 10)
+                
+                //token name
+                VStack (alignment: .leading, spacing: 10) {
+                    Text("Token Name")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    HStack {
+                        TextField("",
+                                  text: $name,
+                                  prompt: Text("Enter token name").foregroundColor(AppColors.secondaryTextColor))
+                    }
+                    .foregroundColor(.white)
+                    .background(AppColors.charcoalColor)
+                    .cornerRadius(SharedLayout.cornerRadius)
+                }
+                
+                //token symbol
+                VStack (alignment: .leading, spacing: 10) {
+                    Text("Token Symbol")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    HStack {
+                        TextField("",
+                                  text: $symbol,
+                                  prompt: Text("Enter token symbol").foregroundColor(AppColors.secondaryTextColor))
+                    }
+                    .foregroundColor(.white)
+                    .background(AppColors.charcoalColor)
+                    .cornerRadius(SharedLayout.cornerRadius)
+                }
+                
+                //token supply
+                VStack (alignment: .leading, spacing: 10) {
+                    Text("Supply")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    HStack {
+                        TextField("",
+                                  text: $supply,
+                                  prompt: Text("Enter token supply").foregroundColor(AppColors.secondaryTextColor))
+                        .keyboardType(.decimalPad)
+                        .onChange(of: supply) {
+                            let filtered = supply.filter { "0123456789".contains($0)}
+                            supply = filtered
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .background(AppColors.charcoalColor)
+                    .cornerRadius(SharedLayout.cornerRadius)
+                }
+                
+                
+                
+                
+                
+                //upload image
+                Button() {
+                    //upload image
+                } label: {
+                    Text("Upload Token Image")
+                        .foregroundColor(.white)
+                }
+                
+                //create token
+                Button() {
+                    //api call for create token
+                } label: {
+                    Text("Create Token")
+                        .foregroundColor(.white)
+                }
+                .disabled(!isFormValid) //if form not valid disable button
+            }
+            .padding()
+        }
     }
 }
 
--- apps/ios/src/App/CreateTokenViewModel.swift
+++ apps/ios/src/App/CreateTokenViewModel.swift
@@ -5,3 +5,25 @@
 //  Created by Gurpreet on 8/5/2026.
 //
 
+import Foundation
+import Combine
+
+class CreateTokenViewModel : ObservableObject {
+    @Published var isCreating = false
+    @Published var errorMessage = ""
+    @Published var creationSuccess = false
+    
+    func createToken(name: String, symbol: String, supply: UInt64, image: Data) async {
+        isCreating = true
+        errorMessage = ""
+        
+        do {
+            try await APIClient.shared.createToken(name: name, symbol: symbol, supply: supply, imageData: image)
+            creationSuccess = true
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+        isCreating = false
+    }
+    
+}
