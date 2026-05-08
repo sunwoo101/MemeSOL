4f364c81b6832ec50e92239a115d6dbb0b935469
gk <198441850+02preet@users.noreply.github.com>
2026-05-08 11:48:33 +1000

    feat: add createtokenview, createtokenviewmodel

---
+++ apps/ios/src/App/CreateTokenView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTokenView.swift
+//  App
+//
+//  Created by Gurpreet on 8/5/2026.
+//
+
+import SwiftUI
+
+struct CreateTokenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTokenView()
+}
---
+++ apps/ios/src/App/CreateTokenViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  CreateTokenViewModel.swift
+//  App
+//
+//  Created by Gurpreet on 8/5/2026.
+//
+
