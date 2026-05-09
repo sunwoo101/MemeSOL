1712a0b5d424a063efeef7738ce57fd39b6b824a
gk <198441850+02preet@users.noreply.github.com>
2026-05-09 18:32:26 +1000

    feat: add search bar

--- apps/ios/src/App/BuyViewModel.swift
+++ apps/ios/src/App/BuyViewModel.swift
@@ -12,6 +12,20 @@ class BuyViewModel : ObservableObject {
     @Published var tokens: [TokenListResponse] = []
     @Published var errorMessage = ""
     
+    @Published var searchText = ""
+    
+    var filteredTokens: [TokenListResponse] {
+        if searchText.isEmpty {
+            return tokens
+        }
+
+        return tokens.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText)
+            ||
+            $0.symbol.localizedCaseInsensitiveContains(searchText)
+        }
+    }
+    
     func loadTokens() async {
         do {
             tokens = try await APIClient.shared.listAllTokens()
