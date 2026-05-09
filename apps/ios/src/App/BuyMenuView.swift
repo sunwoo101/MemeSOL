0b2e4623ee333fca08593ef0bf1f48426506382b
gk <198441850+02preet@users.noreply.github.com>
2026-05-09 17:22:23 +1000

    feat: add buymenuview, buytokenview

---
+++ apps/ios/src/App/BuyMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  BuyMenuView.swift
+//  App
+//
+//  Created by Gurpreet on 9/5/2026.
+//
+
+import SwiftUI
+
+struct BuyMenuView: View {
+    var body: some View {
+        ZStack {
+            AppColors.blackColor.ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    BuyMenuView()
+}
