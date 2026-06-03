fa2412b1746d4cf9a1e3217589a7ef5eacff2720
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2026-06-03 16:34:35 +1000

    feat(backend): image generation

--- apps/backend/Models/DTOs/CreateTokenRequest.cs
+++ apps/backend/Models/DTOs/CreateTokenRequest.cs
@@ -5,5 +5,6 @@ public class CreateTokenRequest
     public required string Name { get; set; }
     public required string Symbol { get; set; }
     public required ulong Supply { get; set; }
-    public required IFormFile Image { get; set; }
+    public IFormFile? Image { get; set; }
+    public bool ImFeelingLucky { get; set; } = false; // If true, the backend will generate a random image instead of using the provided one.
 }
